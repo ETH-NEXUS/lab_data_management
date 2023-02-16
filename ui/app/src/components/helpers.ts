266f4e1f32b2d75513a7c5550b668cfccc4cec23
anastasia-escher <anastasia.escher@gmail.com>
2023-02-16 11:52:38 +0100

    add barcode generator in the ui; add tests to the ui

---
+++ ui/app/src/components/helpers.ts
@@ -0,0 +1,34 @@
+type generalObject = {[key: string]: string}
+
+export const generateBarcodes = (prefix: string, numberOfPlates: number, sides: string[]) => {
+  const shouldIncludeSide: Record<string, boolean> = {}
+  for (const side of sides) {
+    shouldIncludeSide[side] = true
+  }
+
+  return Array.from({length: numberOfPlates}, (_, i) => {
+    const barcode = `${prefix}_${i + 1}$${prefix}_${i + 1}`
+    return {
+      NorthBarcode: shouldIncludeSide['North'] ? barcode : '',
+      SouthBarcode: shouldIncludeSide['South'] ? barcode : '',
+      EastBarcode: shouldIncludeSide['East'] ? barcode : '',
+      WestBarcode: shouldIncludeSide['West'] ? barcode : '',
+    }
+  })
+}
+
+export const downloadCSVData = (columns: Array<string>, items: generalObject[], fileName: string): void => {
+  let csv = columns.join(',')
+  csv += '\r\n'
+
+  for (const row of items) {
+    csv += Object.values(row).join(',')
+    csv += '\r\n'
+  }
+
+  const blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'})
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.download = fileName
+  link.click()
+}
