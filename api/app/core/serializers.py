094136870ea66b9a0fd96ac0629b52a1a81f631c
anastasia-escher <anastasia.escher@gmail.com>
2023-02-20 13:46:44 +0100

    [FIX] delete update method from serializer

--- api/app/core/serializers.py
+++ api/app/core/serializers.py
@@ -174,17 +174,7 @@ class BarcodeSpecificationSerializer(serializers.ModelSerializer):
     model = BarcodeSpecification
     fields = '__all__'
 
-  def update(self, instance, validated_data):
-    print('UPDATING BARCODE SPECIFICATION')
-    try:
-      for attr, value in validated_data.items():
-        print(attr, value)
-        setattr(instance, attr, value)
-      instance.save()
-      return instance
 
-    except Exception as ex:
-      raise Exception(f"Error updating barcode specification: {ex}")
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
