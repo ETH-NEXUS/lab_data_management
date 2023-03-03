cf10c245acb2f84e7499f5ca366618208ef95182
anastasia-escher <anastasia.escher@gmail.com>
2023-03-03 19:41:32 +0100

    refuctor echo mapping function; add tests; add command to export db data as a fixture

--- api/app/importer/management/commands/export.py
+++ api/app/importer/management/commands/export.py
@@ -1,54 +1,55 @@
 from django.core.management.base import BaseCommand
+from django.core import serializers
+from django.db.models import Q
 from friendlylog import colored_logger as log
 import yaml
 from django.apps import apps
+import os
+from os import makedirs
 
 
-def export_data(app_name, model_name, filter_field, filter_value, output_file):
+def export_data(app_name, model_name, _filter=None):
     model = apps.get_model(app_label=app_name, model_name=model_name)
-    if filter_field and filter_value:
-        queryset = model.objects.filter(**{filter_field: filter_value})
+    if _filter:
+        # TODO: Introduce a flexible filter mechanism
+        log.debug(_filter)
+        queryset = model.objects.filter(eval(_filter))
     else:
         queryset = model.objects.all()
 
-    data = []
-    for obj in queryset:
-        fields = {}
-        for field in obj._meta.fields:
-            fields[field.name] = str(field.value_from_object(obj))
-        data.append(fields)
-
-    with open(output_file, 'w') as file:
-        yaml.dump(data, file)
-
-    log.info(f'Data exported successfully to {output_file}')
+    return serializers.serialize("yaml", queryset)
 
 
 class Command(BaseCommand):
-
     help = 'Export data from a Django model to a YAML file'
 
     def add_arguments(self, parser):
         parser.add_argument('app', type=str, help='The app containing the '
                                                   'model to export')
-        parser.add_argument('model', type=str, help='The name of the model '
+        parser.add_argument('model', type=str, help='The name of the '
+                                                        'model '
                                                     'to export')
-        parser.add_argument('--filter', type=str, help='The filter field '
+        parser.add_argument('--filter', '-f', type=str, help='The filter '
+                                                             'field '
                                                        'to apply')
-        parser.add_argument('--filter-value', type=str, help='The value of '
-                                                             'the filter '
-                                                             'field')
-        parser.add_argument('--output-file', type=str, default='data.yaml',
+        parser.add_argument('--append', '-a', action='store_true',
+                            help='Append the result to the existing file')
+
+        parser.add_argument('--output-file', '-o', type=str,
+                            default='test_data/data.yaml',
                             help='The name of the output file')
 
     def handle(self, *args, **options):
         app_name = options.get('app')
         model_name = options.get('model')
         output_file = options.get('output_file')
-        filter_field = options.get('filter')
-        filter_value = options.get('filter_value')
-        if filter_value and filter_field:
-            export_data(app_name, model_name, filter_field, filter_value,
-                        output_file)
-        else:
-            export_data(app_name, model_name, None, None, output_file)
+        _filter = options.get('filter')
+        if _filter and not _filter.startswith('Q('):
+            _filter = f'Q({_filter})'
+        data = export_data(app_name, model_name, _filter)
+
+        makedirs(os.path.split(output_file)[0], exist_ok=True)
+        with open(output_file, 'a' if options.get('append') else 'w') as file:
+            file.write(data)
+
+        log.info(f'Data exported successfully to {output_file}')
