67b19b889b8963d82f33aec6f04f1ae20f7cb07e
anastasia-escher <anastasia.escher@gmail.com>
2023-03-10 14:16:40 +0100

    ad an orange palette

--- ui/app/src/components/data.ts
+++ ui/app/src/components/data.ts
@@ -43,7 +43,8 @@ type Palette = {
 
 export const palettes: Record<string, Palette> = {
   green_red: {val: 'green_red', label: 'Palette 1', from: '#00FF00', to: '#FF0000'},
-  green_brown: {val: 'green_brown', label: 'Palette 2', from: '#b8e186', to: '#8c510a'},
-  blue_red: {val: 'blue_red', label: 'Palette 2', from: '#92c5de', to: '#d6604d'},
-  blue: {val: 'blue', label: 'Palette 4', from: '#92c5de', to: '#0b2746'},
+  green: {val: 'green', label: 'Palette 2', from: '#c7e9c0', to: '#006d2c'},
+  blue: {val: 'blue', label: 'Palette 3', from: '#92c5de', to: '#0b2746'},
+  orange: {val: 'orange', label: 'Palette 4', from: '#fff7bc', to: '#993404'},
+  green_brown: {val: 'green_brown', label: 'Palette 5', from: '#b8e186', to: '#662506'},
 }
