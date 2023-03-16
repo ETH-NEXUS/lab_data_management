f87d95ddb6903d8f83a540d147d9af12d16e70b0
anastasia-escher <anastasia.escher@gmail.com>
2023-03-16 12:37:55 +0100

    [FIX] move bulk_apply_template action to the experiment view

--- ui/app/src/pages/ExperimentPage.vue
+++ ui/app/src/pages/ExperimentPage.vue
@@ -163,7 +163,7 @@ const applyTemplate = async () => {
       $q.loading.show({
         message: t('info.applying_in_progress'),
       })
-      await api.post(`/api/plates/bulk_apply_template/`, {
+      await api.post(`/api/experiments/bulk_apply_template/`, {
         template: selectedTemplatePlateId.value,
         experiment_id: experiment.value.id,
       })
