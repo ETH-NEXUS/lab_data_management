a6c156088e2ec385b3f8ac1444a0db67c2020a6f
anastasia-escher <anastasia.escher@gmail.com>
2023-03-21 16:04:12 +0100

    [ENH] delete HarvestProjectMapping model

--- api/app/core/admin.py
+++ api/app/core/admin.py
@@ -14,7 +14,6 @@ from .models import (
     BarcodeSpecification,
     PlateMapping,
     MeasurementAssignment,
-    HarvestProjectMapping,
 )
 
 
@@ -93,11 +92,6 @@ class LocationAdmin(admin.ModelAdmin):
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ("name",)
-
-
-@admin.register(HarvestProjectMapping)
-class HarvestProjectMappingAdmin(admin.ModelAdmin):
-    list_display = ("name", "harvest_id")
 
 
 @admin.register(Experiment)
