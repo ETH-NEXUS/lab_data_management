c357a7cffa40cee474026f7a0423da85d0c8e411
dameyerdave <dameyerdave@gmail.com>
2023-04-03 16:34:53 +0200

    [FIX] Measurement Timestamps can be null

--- ui/app/src/components/models.ts
+++ ui/app/src/components/models.ts
@@ -130,7 +130,7 @@ export interface PlateDetails {
   id: number
   num_wells: number
   measurement_labels: Array<string>
-  measurement_timestamps: Array<string>
+  measurement_timestamps?: Array<string>
   stats: {[key: string]: {[key: string]: PlateStats}}
   overall_stats: {[key: string]: PlateStats}
 }
