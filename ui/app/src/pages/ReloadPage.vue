9d6624fc9c9ac6aa4c26b2661e48619d78e16f1a
dameyerdave <dameyerdave@gmail.com>
2023-04-25 12:57:23 +0200

    [FIX] Correctly reload notebook after login

---
+++ ui/app/src/pages/ReloadPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fullscreen text-white text-center q-pa-md flex flex-center">
+    <q-spinner-rings color="grey" size="2em" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted} from 'vue'
+
+onMounted(() => {
+  window.location.reload()
+})
+</script>
