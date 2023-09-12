97d09bfaf9c9bbd902026cf23dfcd52e9bea7bb3
dameyerdave <dameyerdave@gmail.com>
2023-09-12 12:40:51 +0200

    [FEAT] Adds management command ldm to refresh materialized views

---
+++ api/app/core/management/commands/ldm.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+import traceback
+
+from core.models import PlateDetail, WellDetail
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("action", type=str, help="The action to execute")
+
+    def refresh_mat_views(self):
+        PlateDetail.refresh(concurrently=True)
+        WellDetail.refresh(concurrently=True)
+
+    def handle(self, *args, **options):
+        try:
+            if options.get("action") == "refresh_mat_views":
+                self.refresh_mat_views()
+        except Exception as ex:
+            print(ex)
+            traceback.print_exc()
