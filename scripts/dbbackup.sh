7e259f967e732f17ee06fecb8eea0fdc8cd857be
anastasia-escher <anastasia.escher@gmail.com>
2023-11-02 15:19:32 +0100

    [FIX] update dbbackup script

--- scripts/dbbackup.sh
+++ scripts/dbbackup.sh
@@ -3,11 +3,28 @@
 
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LOG_FILE="${DIR}/backup.log"
+BACKUP_DIR="/vol/backups"
+FILE_PATTERN="*.backup"
+
 
 function log {
     echo "$(date +'%Y-%m-%d %H:%M:%S') $@" | tee -a "$LOG_FILE"
 }
 
-log "Starting DB Backup..."
-cd "${DIR}/.." && docker-compose exec api /bin/bash -c './manage.py dbbackup' 2>&1
-log "DONE DB Backup."
+
+function db_backup {
+    log "Starting DB Backup..."
+    cd "${DIR}/.." && docker-compose exec api /bin/bash -c './manage.py dbbackup' 2>&1
+    log "DONE DB Backup."
+}
+
+
+function delete_old_backups {
+    log "Deleting backups older than 30 days..."
+    find "$BACKUP_DIR" -type f -name "$FILE_PATTERN" -mtime +30 -exec rm {} \;
+    log "Old backups deleted."
+}
+
+db_backup
+
+delete_old_backups
