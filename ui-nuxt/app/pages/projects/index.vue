eedf412296cab0b2ba4ac88ef212263f31bebc76
anastasia-escher <anastasia.escher@gmail.com>
2026-03-14 14:23:09 +0100

    [ADD] create index.vue with LDM logo display

---
+++ ui-nuxt/app/pages/projects/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ldmLogo from '~/assets/ldm_logo.png'
+</script>
+
+<template>
+  <section class="flex min-h-[calc(100dvh-4rem)] items-center justify-center p-6">
+    <img :src="ldmLogo" alt="LDM Logo" class="h-auto w-[60%] max-w-[860px] object-contain" />
+  </section>
+</template>
