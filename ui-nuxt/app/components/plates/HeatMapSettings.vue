a85d1c1f3c77de57b1dc02264d3e276189b801b8
anastasia-escher <anastasia.escher@gmail.com>
2026-03-15 14:45:55 +0100

    [ADD] add props for square compound type and per plate view toggles in HeatMapSettings

--- ui-nuxt/app/components/plates/HeatMapSettings.vue
+++ ui-nuxt/app/components/plates/HeatMapSettings.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 import { usePlateViewStore } from '~/stores/plateView'
 
+type Props = {
+  showSquareCompoundType?: boolean
+  showPerPlateView?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showSquareCompoundType: true,
+  showPerPlateView: false,
+})
+
 const { t } = useI18n()
 const plateViewStore = usePlateViewStore()
 </script>
@@ -12,9 +22,17 @@ const plateViewStore = usePlateViewStore()
       <span>{{ t('plates.dynamic.settings.smaller_map_view') }}</span>
     </label>
 
-    <label class="inline-flex cursor-pointer items-center gap-2 text-sm text-slate-700">
+    <label
+      v-if="props.showSquareCompoundType"
+      class="inline-flex cursor-pointer items-center gap-2 text-sm text-slate-700"
+    >
       <input v-model="plateViewStore.squareCompoundType" type="checkbox" class="cursor-pointer" />
       <span>{{ t('plates.dynamic.settings.show_wells_as_squares') }}</span>
+    </label>
+
+    <label v-if="props.showPerPlateView" class="inline-flex cursor-pointer items-center gap-2 text-sm text-slate-700">
+      <input v-model="plateViewStore.perPlateView" type="checkbox" class="cursor-pointer" />
+      <span>{{ t('plates.dynamic.settings.per_plate_view') }}</span>
     </label>
   </div>
 </template>
